d05bd3ce8a353f4f0c7a920db6d2dcc09c11c63e
Ashutosh Rai <raiashutech@gmail.com>
2016-04-14 03:54:07 -0400

    practice global variable

---
+++ global_varible.rb
@@ -0,0 +1,22 @@
+###################################
+$gvar=10
+class Class1
+def print_global
+puts "global varible in class 1 is #$gvar"
+end 
+end
+class Class2
+def print_global
+puts "global varible is class 2 is #$gvar"
+end 
+end
+class1obj=Class1.new
+class2obj=Class2.new
+class1obj.print_global()
+class2obj.print_global()
+
+########################################
+
+print "Enter Your Name: "
+name= gets()
+puts "Hello    #{name}"
